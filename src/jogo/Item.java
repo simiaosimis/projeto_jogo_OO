cb8c4a9c45cef45a05a5a80b93352cce03888c61
LucianoAlmeida <mdiebr@gmail.com>
2014-03-28 19:29:40 -0300

    Item.java indentation fix

--- src/jogo/Item.java
+++ src/jogo/Item.java
@@ -4,26 +4,26 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
-public class Item extends Renderizavel{
-	
+public class Item extends Renderizavel {
+
 	private Game game;
 	private BufferedImage item;
-		
-	Item(int x, int y, int altura, int largura, Game game){
-		
-		super(x, y, altura, largura,false,false,false);
-		this.game=game;
-		
+
+	Item(int x, int y, int altura, int largura, Game game) {
+
+		super(x, y, altura, largura, false, false, false);
+		this.game = game;
+
 		SpriteSheet ss = new SpriteSheet(game.getImageitem());
-		
+
 		item = ss.grabImage(1, 1, 38, 42);
 	}
-	
-	public Rectangle getBounds(){
-		return new Rectangle((int)x,(int)y,largura,altura);
+
+	public Rectangle getBounds() {
+		return new Rectangle((int) x, (int) y, largura, altura);
 	}
-	
-	public void render(Graphics g){
-		g.drawImage(item, (int)x, (int)y, null);
+
+	public void render(Graphics g) {
+		g.drawImage(item, (int) x, (int) y, null);
 	}
 }
