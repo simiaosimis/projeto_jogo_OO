4659d332dcfa3a3263a51b8a8ec3012d2a7758bc
MatheusGodinho <matheus_vasco@yahoo.com.br>
2014-04-21 10:39:31 -0300

    Translate the class Collision

---
+++ src/jogo/Collision.java
@@ -0,0 +1,16 @@
+package jogo;
+
+import java.awt.Rectangle;
+
+public interface Collision {
+
+	public boolean collision(Player a, Enemy[] b);
+	
+	public boolean collisionItem(Player a, Item[] b);
+	
+	public int collisionShot(Player a, Enemy[] b);
+	
+	public boolean collisionArrow(Player a, Rectangle b);
+	
+	public boolean collisionTiroPlayer(Player a, Enemy[] b);
+}
